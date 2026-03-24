59fc2c71f32d4604c0e2584d0d077b3d4ee30a72
walid <walidSaoud@gmail.com>
2026-03-24 16:27:21 +0100

    new update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 
-app=Flask(_name_)
+app=Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello DevOps - Docker CI Pipeline!"
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(host="0.0.0.0",port=5000)
